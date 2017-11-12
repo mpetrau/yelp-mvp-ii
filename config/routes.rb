feb5a81bf0c8e065f23b74006179375ddc565de4
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2017-11-12 21:57:04 +0100

    completed basic add restaurant and reviews functionality (index; show; create)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-
-  get 'restaurants/show'
-
-  get 'restaurants/new'
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+      resources :reviews, only: [:create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
